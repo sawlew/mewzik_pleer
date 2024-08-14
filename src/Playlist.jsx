e8e3801cf669d6cf1f442f95020eaf600e3da44d
Soliu Adeosun <sawlew@gmail.com>
2024-08-14 09:29:16 +0100

    Remove song from playlist

--- src/Playlist.jsx
+++ src/Playlist.jsx
@@ -55,10 +55,10 @@ const Playlist = ({ songs, currentSongIndex, setCurrentSongIndex, removeSong }) 
               (
               <svg
                 onClick={(e) => {
-                  e.stopPropagation(); // Prevent triggering the song selection
+                  e.stopPropagation();
                   removeSong(song.originalIndex);
                 }}
-                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 absolute right-1 top-1">
+                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="opacity-40 hover:opacity-100 size-6 absolute right-1 top-1">
                 <path strokeLinecap="round" strokeLinejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
               </svg>
               ) }
