9c1360da45bcb20cd8c820d7faaab4fd6cd2dfc9
Soliu Adeosun <sawlew@gmail.com>
2024-08-14 08:55:15 +0100

    Repeat and shuffle function added

--- src/MusicPlayer.jsx
+++ src/MusicPlayer.jsx
@@ -232,7 +232,7 @@ const MusicPlayer = () => {
             <p className='text-center mt-10'>Playlist is empty</p>
           ) : (
             <>
-            <div className={`bg-[#750e0e9c] absolute w-full bottom-0 backdrop-blur-md transition-all duration-500 ease-in-out ${ fullscreen ? 'h-full' : '' }`}>
+            <div className={`z-10 bg-[#750e0e9c] absolute w-full bottom-0 backdrop-blur-md transition-all duration-500 ease-in-out ${ fullscreen ? 'h-full' : '' }`}>
                 <div className={`max-w-6xl flex justify-center items-center gap-2 p-4 mx-auto
                   ${ fullscreen ? 'h-full flex-col text-center' : '' }
                   `}>
@@ -275,6 +275,7 @@ const MusicPlayer = () => {
                       <span className='text-xs'>{formatTime(duration)}</span>
                     </div>
 
+                    <div className={`flex justify-between ${fullscreen ? 'flex-col' : ''}`}>
                     <div className={`flex gap-2
                       ${fullscreen ? 'justify-center' : ''}
                       `}>
@@ -302,25 +303,48 @@ const MusicPlayer = () => {
                       </button>
                       {/* <button onClick={togglePlaylist}>Show Playlist</button> */}
 
+                      </div>
 
-                      <div className="controls">
-        <button onClick={handleShuffle} className={isShuffle ? 'active' : ''}>
-        {isShuffle ? 'Shuffle On' : 'Shuffle Off'}
-        </button>
-        <button onClick={handleRepeat} className={isRepeat ? 'active' : ''}>
-        {isRepeat ? 'Repeat On' : 'Repeat Off'}
-        </button>
-      </div>
+                    
                     </div>
-                    <div className='absolute flex right-[2%] bottom-4'>
-                      <svg onClick={toggleFullscreen} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={`size-8 active:scale-[1.5] duration-200 ease-linear cursor-pointer ${ fullscreen ? 'hidden' : 'block' }`}>
-                        <path fillRule="evenodd" d="M15 3.75a.75.75 0 0 1 .75-.75h4.5a.75.75 0 0 1 .75.75v4.5a.75.75 0 0 1-1.5 0V5.56l-3.97 3.97a.75.75 0 1 1-1.06-1.06l3.97-3.97h-2.69a.75.75 0 0 1-.75-.75Zm-12 0A.75.75 0 0 1 3.75 3h4.5a.75.75 0 0 1 0 1.5H5.56l3.97 3.97a.75.75 0 0 1-1.06 1.06L4.5 5.56v2.69a.75.75 0 0 1-1.5 0v-4.5Zm11.47 11.78a.75.75 0 1 1 1.06-1.06l3.97 3.97v-2.69a.75.75 0 0 1 1.5 0v4.5a.75.75 0 0 1-.75.75h-4.5a.75.75 0 0 1 0-1.5h2.69l-3.97-3.97Zm-4.94-1.06a.75.75 0 0 1 0 1.06L5.56 19.5h2.69a.75.75 0 0 1 0 1.5h-4.5a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 1 1.5 0v2.69l3.97-3.97a.75.75 0 0 1 1.06 0Z" clipRule="evenodd" />
+                    <div className='absolute flex items-center justify-center gap-2 right-[2%] bottom-3'>
+
+                      <svg onClick={handleShuffle} xmlns="http://www.w3.org/2000/svg" className={`size-9 ${isShuffle ? 'opacity-1' : 'opacity-40'}`} viewBox="0 0 512 512">
+                        <path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={32}
+                          d="M400 304l48 48-48 48M400 112l48 48-48 48M64 352h85.19a80 80 0 0066.56-35.62L256 256" />
+                        <path
+                          d="M64 160h85.19a80 80 0 0166.56 35.62l80.5 120.76A80 80 0 00362.81 352H416M416 160h-53.19a80 80 0 00-66.56 35.62L288 208"
+                          fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={32} />
                       </svg>
 
-                      <svg onClick={toggleFullscreen} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={`size-8 active:scale-[1.5] duration-200 ease-linear cursor-pointer ${ fullscreen ? 'block' : 'hidden' }`}>
-                        <path fillRule="evenodd" d="M3.22 3.22a.75.75 0 0 1 1.06 0l3.97 3.97V4.5a.75.75 0 0 1 1.5 0V9a.75.75 0 0 1-.75.75H4.5a.75.75 0 0 1 0-1.5h2.69L3.22 4.28a.75.75 0 0 1 0-1.06Zm17.56 0a.75.75 0 0 1 0 1.06l-3.97 3.97h2.69a.75.75 0 0 1 0 1.5H15a.75.75 0 0 1-.75-.75V4.5a.75.75 0 0 1 1.5 0v2.69l3.97-3.97a.75.75 0 0 1 1.06 0ZM3.75 15a.75.75 0 0 1 .75-.75H9a.75.75 0 0 1 .75.75v4.5a.75.75 0 0 1-1.5 0v-2.69l-3.97 3.97a.75.75 0 0 1-1.06-1.06l3.97-3.97H4.5a.75.75 0 0 1-.75-.75Zm10.5 0a.75.75 0 0 1 .75-.75h4.5a.75.75 0 0 1 0 1.5h-2.69l3.97 3.97a.75.75 0 1 1-1.06 1.06l-3.97-3.97v2.69a.75.75 0 0 1-1.5 0V15Z" clipRule="evenodd" />
-                      </svg>
+                      {isRepeat ? 
+                      (<svg onClick={handleRepeat} fill='white' className='size-8' 
+                        height={32} viewBox="0 0 32 32" width={32} xmlns="http://www.w3.org/2000/svg">
+                        <path d="m6 6h20.1719l-3.586-3.5859 1.4141-1.4141 6 6-6 6-1.4141-1.4141 3.586-3.5859h-20.1719v7h-2v-7a2.0024 2.0024 0 0 1 2-2z" />
+                        <path d="m9.4141 20.4141-3.586 3.5859h20.1719v-7h2v7a2.0024 2.0024 0 0 1 -2 2h-20.1719l3.5859 3.5859-1.414 1.4141-6-6 6-6z" />
+                        <path d="m17 19v-8h-2v1h-2v2h2v5h-2v2h6v-2z" />
+                        <path d="m0 0h32v32h-32z" fill="none" />
+                      </svg>)
+                      : 
+                      (<svg onClick={handleRepeat} fill='white' className="size-8"
+                        height={32} viewBox="0 0 32 32" width={32} xmlns="http://www.w3.org/2000/svg">
+                      <path d="m6 6h20.1719l-3.586-3.5859 1.4141-1.4141 6 6-6 6-1.4141-1.4141 3.586-3.5859h-20.1719v7h-2v-7a2.0024 2.0024 0 0 1 2-2z" />
+                      <path d="m9.4141 20.4141-3.586 3.5859h20.1719v-7h2v7a2.0024 2.0024 0 0 1 -2 2h-20.1719l3.5859 3.5859-1.414 1.4141-6-6 6-6z" />
+                      <path d="m0 0h32v32h-32z" fill="none" />
+                      </svg>)
+                      }
+            
+                      <div className='flex justify-center'>
+                        <svg onClick={toggleFullscreen} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={`size-8 active:scale-[1.5] duration-200 ease-linear cursor-pointer ${ fullscreen ? 'hidden' : 'block' }`}>
+                          <path fillRule="evenodd" d="M15 3.75a.75.75 0 0 1 .75-.75h4.5a.75.75 0 0 1 .75.75v4.5a.75.75 0 0 1-1.5 0V5.56l-3.97 3.97a.75.75 0 1 1-1.06-1.06l3.97-3.97h-2.69a.75.75 0 0 1-.75-.75Zm-12 0A.75.75 0 0 1 3.75 3h4.5a.75.75 0 0 1 0 1.5H5.56l3.97 3.97a.75.75 0 0 1-1.06 1.06L4.5 5.56v2.69a.75.75 0 0 1-1.5 0v-4.5Zm11.47 11.78a.75.75 0 1 1 1.06-1.06l3.97 3.97v-2.69a.75.75 0 0 1 1.5 0v4.5a.75.75 0 0 1-.75.75h-4.5a.75.75 0 0 1 0-1.5h2.69l-3.97-3.97Zm-4.94-1.06a.75.75 0 0 1 0 1.06L5.56 19.5h2.69a.75.75 0 0 1 0 1.5h-4.5a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 1 1.5 0v2.69l3.97-3.97a.75.75 0 0 1 1.06 0Z" clipRule="evenodd" />
+                        </svg>
+
+                        <svg onClick={toggleFullscreen} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={`size-8 active:scale-[1.5] duration-200 ease-linear cursor-pointer ${ fullscreen ? 'block' : 'hidden' }`}>
+                          <path fillRule="evenodd" d="M3.22 3.22a.75.75 0 0 1 1.06 0l3.97 3.97V4.5a.75.75 0 0 1 1.5 0V9a.75.75 0 0 1-.75.75H4.5a.75.75 0 0 1 0-1.5h2.69L3.22 4.28a.75.75 0 0 1 0-1.06Zm17.56 0a.75.75 0 0 1 0 1.06l-3.97 3.97h2.69a.75.75 0 0 1 0 1.5H15a.75.75 0 0 1-.75-.75V4.5a.75.75 0 0 1 1.5 0v2.69l3.97-3.97a.75.75 0 0 1 1.06 0ZM3.75 15a.75.75 0 0 1 .75-.75H9a.75.75 0 0 1 .75.75v4.5a.75.75 0 0 1-1.5 0v-2.69l-3.97 3.97a.75.75 0 0 1-1.06-1.06l3.97-3.97H4.5a.75.75 0 0 1-.75-.75Zm10.5 0a.75.75 0 0 1 .75-.75h4.5a.75.75 0 0 1 0 1.5h-2.69l3.97 3.97a.75.75 0 1 1-1.06 1.06l-3.97-3.97v2.69a.75.75 0 0 1-1.5 0V15Z" clipRule="evenodd" />
+                        </svg>
+                      </div>
                     </div>
+                   
                   </div>
                   
                 </div>
--- src/Playlist.jsx
+++ src/Playlist.jsx
@@ -38,7 +38,7 @@ const Playlist = ({ songs, currentSongIndex, setCurrentSongIndex, removeSong }) 
             <li
               key={index}
               onClick={() => setCurrentSongIndex(song.originalIndex)}
-              className={`flex items-center p-4 gap-4 transition-all duration-300 rounded-lg cursor-pointer hover:bg-[#9c1313]
+              className={`relative flex items-center p-4 gap-4 transition-all duration-300 rounded-lg cursor-pointer hover:bg-[#9c1313]
                 ${ song.originalIndex === currentSongIndex ? 'font-bold bg-[#9c1313]' : 'font-normal bg-[#750e0e9c]' }`}
             >
               <img 
@@ -50,7 +50,21 @@ const Playlist = ({ songs, currentSongIndex, setCurrentSongIndex, removeSong }) 
                 <p className=''>{song.title}</p>
                 <p className=''>{song.artist}</p>
               </div>
-              <button
+              { song.originalIndex === currentSongIndex ? '' : 
+              
+              (
+              <svg
+                onClick={(e) => {
+                  e.stopPropagation(); // Prevent triggering the song selection
+                  removeSong(song.originalIndex);
+                }}
+                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 absolute right-1 top-1">
+                <path strokeLinecap="round" strokeLinejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+              </svg>
+              ) }
+              
+
+              {/* <button
                   onClick={(e) => {
                     e.stopPropagation(); // Prevent triggering the song selection
                     removeSong(song.originalIndex);
@@ -58,7 +72,7 @@ const Playlist = ({ songs, currentSongIndex, setCurrentSongIndex, removeSong }) 
                   className="bg-red-500 text-white p-2 rounded-md"
                 >
                   Remove
-                </button>
+                </button> */}
             </li>
           ))}
         </ul>
