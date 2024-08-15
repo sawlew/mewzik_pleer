ca52c3e78f36382af53cc0ebf651a737b0e9e550
Soliu Adeosun <sawlew@gmail.com>
2024-08-15 09:13:24 +0100

    Modified the shuffle and repeat function

--- src/MusicPlayer.jsx
+++ src/MusicPlayer.jsx
@@ -148,11 +148,29 @@ const MusicPlayer = () => {
     }
   };
 
+  // const playPrev = () => {
+  //   setCurrentSongIndex((currentSongIndex - 1 + songs.length) % songs.length);
+  //   setShouldPlay(true);
+  //   setIsPlaying(true);
+  // };
+
   const playPrev = () => {
-    setCurrentSongIndex((currentSongIndex - 1 + songs.length) % songs.length);
+    if (isRepeat) {
+      audioRef.current.currentTime = 0;
+      audioRef.current.play();
+    } else {
+      let prevIndex;
+      if (isShuffle) {
+        prevIndex = Math.floor(Math.random() * songs.length);
+      } else {
+        prevIndex = (currentSongIndex - 1 + songs.length) % songs.length;
+      }
+      setCurrentSongIndex(prevIndex);
+    }
     setShouldPlay(true);
     setIsPlaying(true);
   };
+  
 
   const handleTimeUpdate = () => {
     if (!audioRef.current) return;
@@ -301,7 +319,6 @@ const MusicPlayer = () => {
                           <path d="M5.055 7.06C3.805 6.347 2.25 7.25 2.25 8.69v8.122c0 1.44 1.555 2.343 2.805 1.628L12 14.471v2.34c0 1.44 1.555 2.343 2.805 1.628l7.108-4.061c1.26-.72 1.26-2.536 0-3.256l-7.108-4.061C13.555 6.346 12 7.249 12 8.689v2.34L5.055 7.061Z" />
                         </svg>
                       </button>
-                      {/* <button onClick={togglePlaylist}>Show Playlist</button> */}
 
                       </div>
 
